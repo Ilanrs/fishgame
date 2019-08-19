1ce87f6d795a24bca9555057ce30a6f7baf76bdf
cwklinus22015 <student+22015@codingwithkids.com>
2019-08-19 15:52:20 -0700

    started coding size, screen

--- main1.py
+++ main1.py
@@ -4,3 +4,16 @@ def main ()
 
 if __name__ == "__main__"
     main()
+
+
+import sys, pygame
+from pygame.locals import*
+
+pygame.init()
+screen_info = pygame.display.Info()
+
+
+size = (width, height)v = (int(screen_info.current_w), int(screen_info.current_h))
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+color = (0,127,255)
